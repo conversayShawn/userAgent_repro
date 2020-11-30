6f7e6e11334f8b461ad130d9fb1104ff5fca404c
Yuto Takamuro <ytakamuro@fandango.com>
2020-11-30 12:48:07 -0800

    adding example

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,21 +1,38 @@
-/// <reference types="cypress" />
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const configOverride = {};
 
-/**
- * @type {Cypress.PluginConfig}
- */
+function setUserAgent(defaultConfig) {
+    const ua = defaultConfig.env.userAgent
+
+    switch (ua) {
+        case 'desktop':
+            configOverride.userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36';
+            configOverride.testFiles = 'desktop/**.spec.js';
+            configOverride.viewportWidth = 1920;
+            configOverride.viewportHeight = 1080;
+            break;
+
+        case 'mobile':
+            configOverride.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1';
+            configOverride.testFiles = 'mobile/**.spec.js';
+            configOverride.viewportWidth = 375;
+            configOverride.viewportHeight = 812;
+            break;
+
+        // case 'tablet': 
+
+        default:
+            configOverride.userAgent = null;
+    }
+}
+
+
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+    setUserAgent(config)
+
+    return Object.assign({}, config, configOverride);
 }
