aea6da34e4499bae47ac6290715bacf107c34054
Yuto Takamuro <ytakamuro@fandango.com>
2020-11-30 12:55:48 -0800

    simplify and add redundant case for clarity

--- cypress/integration/mobile/mobile.spec.js
+++ cypress/integration/mobile/mobile.spec.js
@@ -14,6 +14,16 @@ describe('sample', () => {
             .should('be.visible')
     })
 
+    it('can click Spotlight to land on mobile home page', () => {
+        cy.get('.nav-bar').within(() => {
+            cy.contains('Spotlight')
+                .click({ force: true })
+        })
+
+        cy.get('#now-playing-section') // this is a mobile-only element
+            .should('be.visible')
+    })
+
     it('can click Movies to land on mobile /movies-in-theaters', () => {
         cy.get('.nav-bar').within(() => {
             cy.contains('Movies')
