f88628e4a833f2197d288c38ad884191a8f81eb0
Shawn Harris <shawn.harris3510@gmail.com>
2023-02-27 22:10:40 -0800

    add desktop tests

--- cypress/e2e/mobile/mobile.cy.js
+++ cypress/e2e/mobile/mobile.cy.js
@@ -6,31 +6,31 @@ describe('sample', () => {
 
     it('can click Spotlight to land on mobile home page', () => {
         cy.get('.nav-bar').within(() => {
-            cy.contains('Spotlight')
+            cy.contains('Spotlight') // this is a mobile-only element
                 .click({ force: true })
         })
 
-        cy.get('#now-playing-section') // this is a mobile-only element
+        cy.get('#now-playing-section')
             .should('be.visible')
     })
 
     it('can click Spotlight to land on mobile home page', () => {
         cy.get('.nav-bar').within(() => {
-            cy.contains('Spotlight')
+            cy.contains('Spotlight') // this is a mobile-only element
                 .click({ force: true })
         })
 
-        cy.get('#now-playing-section') // this is a mobile-only element
+        cy.get('#now-playing-section')
             .should('be.visible')
     })
 
     it('can click Movies to land on mobile /movies-in-theaters', () => {
         cy.get('.nav-bar').within(() => {
-            cy.contains('Spotlight')
+            cy.contains('Spotlight') // this is a mobile-only element
                 .click({ force: true })
         })
 
-        cy.get('#now-playing-section') // this is a mobile-only element
+        cy.get('#now-playing-section')
             .should('be.visible')
     })
 })
